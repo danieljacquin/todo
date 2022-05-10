23512a296d81adc27e41d7b4d64eb4c85ed692cc
danieljacquin <danielcamilo853@gmail.com>
2022-05-09 19:04:43 -0500

    sonarcloud file is updated again 16

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World Jacquin!"', () => {
-      expect(appController.getHello()).toBe('Hello World Jacquin!');
+      expect(appController.getHello()).toBe('Hello World daniel!');
     });
   });
 });
